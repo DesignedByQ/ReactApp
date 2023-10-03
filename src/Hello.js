c6c560ebb34f4ffecda68a4ac404739e24058e17
Henry Opara <henry.o@hotmail.co.uk>
2023-10-03 14:03:06 +0100

    map feature

--- src/Hello.js
+++ src/Hello.js
@@ -5,12 +5,20 @@ var Hello = () => {
     var i = 10;
     var j = 20;
 
+    var names = ["allan", "bob", "james"];
+
     return(<div>
 
             <h3>Hello component...</h3>
             <p>This is my first application...</p>
             {i}
             {i+j}
+
+            <h3>names</h3>
+
+            {names.map((n) => {
+                return <p>{n}</p>
+            })}
         
         </div>)
 }   
