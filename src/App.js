34eca9649d314efe8ca01d458822570f5394883c
Henry Opara <henry.o@hotmail.co.uk>
2023-10-03 12:51:14 +0100

    setup component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Hello from './Hello';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          blah blah 111
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello react!!</h1>
+      <Hello></Hello>
     </div>
   );
 }
---
+++ src/Hello.js
@@ -0,0 +1,18 @@
+
+
+var Hello = () => {
+
+    var i = 10;
+    var j = 20;
+
+    return(<div>
+
+            <h3>Hello component...</h3>
+            <p>This is my first application...</p>
+            {i}
+            {i+j}
+        
+        </div>)
+}   
+
+export default Hello;
